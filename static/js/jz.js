afaeab188ea7cab94e32d607c2f9f6833ca934a6
Jake Zerrer <him@jakezerrer.com>
2015-09-27 23:33:45 -0400

    Remove unused javascript

--- static/js/jz.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-
-  var backgroundCanvas = document.getElementById('background');
-
-  b_context = backgroundCanvas.getContext("2d");
-  b_context.fillRect(50, 25, 250, 100);
-  console.log('hi')
-});
